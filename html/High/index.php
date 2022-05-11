79e2ee33e31211b6aadbb065d8144f8a9a221c42
iewilmaestro <83399918+iewilmaestro@users.noreply.github.com>
2022-05-11 16:45:01 +0700

    Create index.php

---
+++ html/High/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>iewil | About</title>
+	<link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+	<div id="header">
+		<ul>
+			<li style="color: white;font-size: 25px;font-weight: bold"><marquee direction="right">Welcome to Iewil Website</marquee>Menu &raquo;</li>
+			<li><a href="../">Dashboard</a></li>
+			<li><a href="../List">List</a></li>
+			<li><a href="">About</a></li>
+		</ul>
+	</div>
+
+	<div id="content">
+	    <a href="https://litefaucet.in/Solana/?r=purna.iera@gmail.com" style="background-color:red;color:white">Claim 3000 Solana Every 1 Minutes</a>
+	    <a href="https://claimliteco.in/?r=purna.iera@gmail.com" style="background-color:blue;color:white">Claim 2500 Litecoin Every 1 Minutes</a>
+	    
+    </div>
+	<div id="footer">
+    	Copyright &copy; iewil
+	</div>
+</body>
+</html>
