4fa77932ab76c97f0a626884179827feb04902d0
iewilmaestro <83399918+iewilmaestro@users.noreply.github.com>
2024-03-04 11:37:47 +0700

    Delete html directory

--- html/Link/Link.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_GET["session"])){
-    session_start();
-    $_SESSION["id"]=$_GET["session"];
-    header('Location: ../');
-}else{
-    echo "MAU NGAPAIN COK";
-}
-?>
--- html/List/backup.php
+++
@@ -1,4 +0,0 @@
-<?php 
-require "functions.php";
-$list = query("SELECT * FROM List ORDER BY NamaSitus ASC");
-print_r(json_encode($list));
